8ed098ea922c918963ddad834a59e3eb539f0ddc
Jorrit Poelen <jhpoelen@gmail.com>
2015-08-21 18:12:57 -0700

    add base64 data urls

--- test/imageFor.js
+++ test/imageFor.js
@@ -6,7 +6,18 @@ test('image for mammals', function(t) {
     t.equal(taxaprisma.imageFor('Animalia Mammalia Homo Sapiens'), 'http://phylopic.org/assets/images/submissions/e5b0cde8-beab-48dc-b77c-d48b16c6a05e.thumb.png');
 });
 
+test('image data url for mammals', function(t) {
+    t.plan(1);
+    t.ok(taxaprisma.imageDataUrlFor('Animalia Mammalia Homo Sapiens').match(/^data:image\/png;base64,/));
+});
+
 test('default image', function(t) {
     t.plan(1);
     t.notOk(taxaprisma.imageFor('Donald duckus'));
 });
+
+test('default image', function(t) {
+    t.plan(1);
+    t.notOk(taxaprisma.imageDataUrlFor('Donald duckus'));
+});
+
---
+++ test/imageLicenseFor.js
@@ -0,0 +1,17 @@
+var taxaprisma = require('../');
+var test = require('tape');
+
+test('image license for mammals', function(t) {
+    t.plan(1);
+    t.equal(taxaprisma.imageLicenseFor('Animalia Mammalia Homo Sapiens'), 'http://creativecommons.org/publicdomain/zero/1.0/');
+});
+
+test('image license for mollusks', function(t) {
+    t.plan(1);
+    t.equal(taxaprisma.imageLicenseFor('Something Mollusca something'), 'by Marcello under http://creativecommons.org/licenses/by/3.0/');
+});
+
+test('default image', function(t) {
+    t.plan(1);
+    t.notOk(taxaprisma.imageLicenseFor('Donald duckus'));
+});

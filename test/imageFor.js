026ae21689f3485791abb959b246a5e7b056f016
Jorrit Poelen <jhpoelen@gmail.com>
2015-08-26 11:08:51 -0700

    correct inline picture for birds as suggested by @coding46

--- test/imageFor.js
+++ test/imageFor.js
@@ -6,9 +6,19 @@ test('image for mammals', function(t) {
     t.equal(taxaprisma.imageFor('Animalia Mammalia Homo Sapiens'), 'http://phylopic.org/assets/images/submissions/e5b0cde8-beab-48dc-b77c-d48b16c6a05e.thumb.png');
 });
 
+test('image for birds', function(t) {
+    t.plan(1);
+    t.equal(taxaprisma.imageFor('Animalia Aves Larus'), 'http://phylopic.org/assets/images/submissions/ee764929-c865-44f6-b5db-b4e7d5693d1a.thumb.png');
+});
+
 test('image data url for mammals', function(t) {
     t.plan(1);
     t.ok(taxaprisma.imageDataUrlFor('Animalia Mammalia Homo Sapiens').match(/^data:image\/png;base64,/));
+});
+
+test('image data url for aves', function(t) {
+    t.plan(1);
+    t.ok(taxaprisma.imageDataUrlFor('Animalia Aves Larus').match(/SuQmCC/));
 });
 
 test('default image', function(t) {

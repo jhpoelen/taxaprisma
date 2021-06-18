fd5f92e33aeedc25675c364960c36ddb6b4e6c84
jhpoelen <jhpoelen@xs4all.nl>
2021-06-18 08:15:51 -0700

    add support for Viridiplantae

--- test/colorFor.js
+++ test/colorFor.js
@@ -6,6 +6,16 @@ test('color for mammals', function(t) {
     t.equal(taxaprisma.colorFor('Animalia Mammalia Homo Sapiens'), '#1E90FF');
 });
 
+test('color for plants', function(t) {
+    t.plan(1);
+    t.equal(taxaprisma.colorFor('Viridiplantae'), '#73AC13');
+});
+
+test('another color for plants', function(t) {
+    t.plan(1);
+    t.equal(taxaprisma.colorFor('Plantae'), '#73AC13');
+});
+
 test('default color', function(t) {
     t.plan(1);
     t.equal(taxaprisma.colorFor('Donald duckus'), '#000000');
